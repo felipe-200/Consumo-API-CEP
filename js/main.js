55807d97385b426dfac1fca17c6fe521fe8f8616
git/ <jailsonsilvaoto@gmail.com>
2022-05-18 11:14:22 -0300

    Finalizando

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 const pesquisarCep = async() => {
     limparFormulario();
     const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
     if (cepValido(cep)){
         // usando a ferramenta fetch API
         const dados = await fetch(url); // recebendo o resultado do fetch
